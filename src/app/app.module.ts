8be769f5f7b8b292ac0bab0103eb1a9f5220f674
José Nunes <jose.nunes@glintt.com>
2020-11-13 21:05:37 +0000

    Added style and form behavior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { ConinhasFormComponent } from './components/coninhas-form/coninhas-form.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/coninhas-form/coninhas-form.component.ts
+++ src/app/components/coninhas-form/coninhas-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-coninhas-form',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConinhasFormComponent implements OnInit {
 
-  constructor() { }
+  public reportForm: FormGroup = new FormGroup({
+    date: new FormControl(formatDate(new Date(), 'yyyy-MM-dd', this.locale)),
+    hour: new FormControl(formatDate(new Date(), 'HH:mm', this.locale)),
+    offense: new FormControl(null),
+    tears: new FormControl(null),
+    forget: new FormControl(null),
+    reasons: new FormGroup({
+      pussy: new FormControl(false),
+      envy: new FormControl(false),
+      idiot: new FormControl(false),
+      sensible: new FormControl(false),
+      moral_superior: new FormControl(false),
+      blah: new FormControl(false),
+      sexualy_frustrated: new FormControl(false),
+      no_likes: new FormControl(false),
+      puritane: new FormControl(false),
+      shitty_life: new FormControl(false),
+      miss_mom: new FormControl(false),
+      another: new FormControl(false),
+    })
+  });
 
-  ngOnInit(): void {
+  constructor(@Inject(LOCALE_ID) private locale: string) {
   }
 
+  ngOnInit(): void {
+    this.reportForm.valueChanges.subscribe(form => {
+      console.log(form);
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.reportForm.value);
+  }
 }
